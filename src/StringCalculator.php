5e438b6fecf5a28cd9f52b6ea0d096b3e2177437
alvaro larraya <larraya.132169@e.unavarra.es>
2023-02-21 20:27:19 +0100

    Commit inicial

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,5 +4,25 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
-    // TODO: String Calculator Kata
+
+    public function __construct()
+    {
+    }
+
+    function add(string $numbers): int
+    {
+        if(empty($numbers)){
+            return 0;
+        }
+        if(!str_contains($numbers,',')){
+            return intval($numbers);
+        }else{
+            $numbers = explode(",",str_replace("\n",",",$numbers));
+            $result = 0;
+            foreach ($numbers as $number){
+                $result += intval($number);
+            }
+            return $result;
+        }
+    }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -9,5 +9,60 @@ use PHPUnit\Framework\TestCase;
 
 final class StringCalculatorTest extends TestCase
 {
-    // TODO: String Calculator Kata Tests
+    /**
+     * @test
+     */
+    public function shouldReturn0ToEmptyString()
+    {
+        //preparacion
+        $calculator = new StringCalculator();
+        //ejecucion
+        $result = $calculator->add("");
+        //validacion
+        $this->assertEquals(0, $result);
+    }
+    /**
+     * @test
+     */
+    public function shouldReturnTheNumberIfItComesAlone()
+    {
+        $calculator = new StringCalculator();
+
+        $result = $calculator->add("4");
+
+        $this->assertEquals(4, $result);
+    }
+    /**
+     * @test
+     */
+    public function shouldReturnTwoNumbersAdded()
+    {
+        $calculator = new StringCalculator();
+
+        $result = $calculator->add("1,2");
+
+        $this->assertEquals(3, $result);
+    }
+    /**
+     * @test
+     */
+    public function shouldReturnAllNumbersAdded()
+    {
+        $calculator = new StringCalculator();
+
+        $result = $calculator->add("1,2,3,4");
+
+        $this->assertEquals(10, $result);
+    }
+    /**
+     * @test
+     */
+    public function shouldReturnAllNumbersAddedWithLineBreakAsSeparator()
+    {
+        $calculator = new StringCalculator();
+
+        $result = $calculator->add("1,2\n3\n4");
+
+        $this->assertEquals(10, $result);
+    }
 }
