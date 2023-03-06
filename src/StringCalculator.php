f11274540240ace855c528f113f4c18e054ce985
alvaro larraya <larraya.132169@e.unavarra.es>
2023-03-06 22:11:22 +0100

    testing github actions that runs grumphp with line 19 error on else solved

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,25 +4,23 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
-
     public function __construct()
     {
     }
 
-    function add(string $numbers): int
+    public function add(string $numbers): int
     {
-        if(empty($numbers)){
+        if (empty($numbers)) {
             return 0;
         }
-        if(!str_contains($numbers,',')){
+        if (!str_contains($numbers, ',')) {
             return intval($numbers);
-        }else{
-            $numbers = explode(",",str_replace("\n",",",$numbers));
-            $result = 0;
-            foreach ($numbers as $number){
-                $result += intval($number);
-            }
-            return $result;
         }
+        $numbers = explode(",", str_replace("\n", ",", $numbers));
+        $result = 0;
+        foreach ($numbers as $number) {
+            $result += intval($number);
+        }
+        return $result;
     }
 }
